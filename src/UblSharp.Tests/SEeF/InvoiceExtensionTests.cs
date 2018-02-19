42d91853ba7d58cca103cb7e36c165763f8ba627
Remco Ros <remcoros@live.nl>
2018-02-19 23:16:58 +0100

    Show example/unit test of serializing custom extensions for #14

--- src/UblSharp.Tests/SEeF/InvoiceExtensionTests.cs
+++ src/UblSharp.Tests/SEeF/InvoiceExtensionTests.cs
@@ -1,13 +1,25 @@
-﻿using System.IO;
+﻿using System.Collections.Generic;
+using System.IO;
+using System.Text;
+using System.Xml;
 using FluentAssertions;
+using UblSharp.CommonExtensionComponents;
 using UblSharp.SEeF;
 using UblSharp.Tests.Util;
 using Xunit;
+using Xunit.Abstractions;
 
 namespace UblSharp.Tests.SEeF
 {
     public class InvoiceExtensionTests
     {
+        private readonly ITestOutputHelper _output;
+
+        public InvoiceExtensionTests(ITestOutputHelper output)
+        {
+            _output = output;
+        }
+
         [Fact]
         public void CanDeserializeSEeF_V1()
         {
@@ -41,6 +53,71 @@ namespace UblSharp.Tests.SEeF
 
                 seef.UtilityConsumptionPoint.Should().NotBeEmpty();
                 seef.UtilityConsumptionPoint[0].ID.Value.Should().Be("871687400001234567");
+            }
+        }
+
+        [Fact]
+        public void CanSerializeSEeF_V1()
+        {
+            var xmlSerializer = UblSharp.SEeF.XmlSerializerFactory.Default.GetSerializer(SEeFVersion.V1);
+
+            var seef = new SEEFExtensionWrapperType()
+            {
+                UtilityConsumptionPoint =
+                {
+                    new ConsumptionPointType()
+                    {
+                        ID = "ConsumptionPointType_ID"
+                    }
+                }
+            };
+
+            // Create a temporary XmlDocument
+            var doc = new XmlDocument();
+
+            // Create an XmlWriter which writes elements to this document
+            using (var writer = doc.CreateNavigator().AppendChild())
+            {
+                // Serialize our custom type to this XmlWriter
+                xmlSerializer.Serialize(writer, seef);
+            }
+
+            // Get the root XmlElement of our custom document and use it as our ExtensionContent
+            var ext = new UBLExtensionType()
+            {
+                ExtensionContent = doc.DocumentElement
+            };
+
+            var invoice = new InvoiceType()
+            {
+                ID = "id",
+                UBLExtensions = new List<UBLExtensionType>()
+                {
+                    ext
+                }
+            };
+
+            var sb = new StringBuilder();
+            using (var sw = new StringWriter(sb))
+            {
+                UblDocument.Save(invoice, sw);
+
+                sb.ToString().Should().Be(
+                    @"<?xml version=""1.0"" encoding=""utf-16""?>
+<Invoice xmlns:xsd=""http://www.w3.org/2001/XMLSchema"" xmlns:xsi=""http://www.w3.org/2001/XMLSchema-instance"" xmlns:cac=""urn:oasis:names:specification:ubl:schema:xsd:CommonAggregateComponents-2"" xmlns:cbc=""urn:oasis:names:specification:ubl:schema:xsd:CommonBasicComponents-2"" xmlns=""urn:oasis:names:specification:ubl:schema:xsd:Invoice-2"">
+	<UBLExtensions xmlns=""urn:oasis:names:specification:ubl:schema:xsd:CommonExtensionComponents-2"">
+		<UBLExtension xmlns:sig=""urn:oasis:names:specification:ubl:schema:xsd:CommonSignatureComponents-2"" xmlns:sbc=""urn:oasis:names:specification:ubl:schema:xsd:SignatureBasicComponents-2"">
+			<ExtensionContent>
+				<SEEFExtensionWrapper xmlns=""urn:www.energie-efactuur.nl:profile:invoice:ver1.0"">
+					<UtilityConsumptionPoint>
+						<ID xmlns=""urn:oasis:names:specification:ubl:schema:xsd:CommonBasicComponents-2"">ConsumptionPointType_ID</ID>
+					</UtilityConsumptionPoint>
+				</SEEFExtensionWrapper>
+			</ExtensionContent>
+		</UBLExtension>
+	</UBLExtensions>
+	<cbc:ID>id</cbc:ID>
+</Invoice>");
             }
         }
     }
---
+++ src/UblSharp.Tests/UseCases/CustomExtensionTests.cs
@@ -0,0 +1,86 @@
+﻿using System.Collections.Generic;
+using System.IO;
+using System.Text;
+using System.Xml;
+using System.Xml.Serialization;
+using FluentAssertions;
+using UblSharp.CommonExtensionComponents;
+using UblSharp.CoreComponentTypes;
+using Xunit;
+using Xunit.Abstractions;
+
+namespace UblSharp.Tests.UseCases
+{
+    // Define 'ValueType' because they are not defined in UblSharp (they are abstracted away to their 'Core component type')
+    [XmlType("Value", Namespace = "urn:oasis:names:specification:ubl:schema:xsd:CommonBasicComponents-2")]
+    [XmlRoot(Namespace = "urn:oasis:names:specification:ubl:schema:xsd:CommonBasicComponents-2", IsNullable = true)]
+    public class ValueType : CctTextType
+    {
+    }
+
+    public class CustomExtensionTests
+    {
+        private readonly ITestOutputHelper _output;
+
+        public CustomExtensionTests(ITestOutputHelper output)
+        {
+            _output = output;
+        }
+
+        [Fact]
+        public void CanSerializeCustomCbcType()
+        {
+            var xmlSerializer = new XmlSerializer(typeof(ValueType));
+
+            var value = new ValueType()
+            {
+                Value = "SomeValue"
+            };
+
+            // Create a temporary XmlDocument
+            var doc = new XmlDocument();
+
+            // Create an XmlWriter which writes elements to this document
+            using (var writer = doc.CreateNavigator().AppendChild())
+            {
+                // Serialize our custom type to this XmlWriter
+                xmlSerializer.Serialize(writer, value);
+            }
+
+            // Get the root XmlElement of our custom document and use it as our ExtensionContent
+            var ext = new UBLExtensionType()
+            {
+                ExtensionContent = doc.DocumentElement
+            };
+
+            var invoice = new InvoiceType()
+            {
+                ID = "id",
+                UBLExtensions = new List<UBLExtensionType>()
+                {
+                    ext
+                }
+            };
+
+            var sb = new StringBuilder();
+            using (var sw = new StringWriter(sb))
+            {
+                UblDocument.Save(invoice, sw);
+
+                sb.ToString().Should().Be(
+                    @"<?xml version=""1.0"" encoding=""utf-16""?>
+<Invoice xmlns:xsd=""http://www.w3.org/2001/XMLSchema"" xmlns:xsi=""http://www.w3.org/2001/XMLSchema-instance"" xmlns:cac=""urn:oasis:names:specification:ubl:schema:xsd:CommonAggregateComponents-2"" xmlns:cbc=""urn:oasis:names:specification:ubl:schema:xsd:CommonBasicComponents-2"" xmlns=""urn:oasis:names:specification:ubl:schema:xsd:Invoice-2"">
+	<UBLExtensions xmlns=""urn:oasis:names:specification:ubl:schema:xsd:CommonExtensionComponents-2"">
+		<UBLExtension xmlns:sig=""urn:oasis:names:specification:ubl:schema:xsd:CommonSignatureComponents-2"" xmlns:sbc=""urn:oasis:names:specification:ubl:schema:xsd:SignatureBasicComponents-2"">
+			<ExtensionContent>
+				<Value xmlns=""urn:oasis:names:specification:ubl:schema:xsd:CommonBasicComponents-2"">SomeValue</Value>
+			</ExtensionContent>
+		</UBLExtension>
+	</UBLExtensions>
+	<cbc:ID>id</cbc:ID>
+</Invoice>");
+            }
+        }
+
+    }
+}
